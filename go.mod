f209c8f366fcec154db3da03f18d66b9bb19763b
Kevin Chen <kevinmichaelchen@gmail.com>
2020-05-17 22:04:53 -0400

    GetTraceInfo

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/AlpacaLabs/go-kontext
 go 1.14
 
 require (
+	github.com/AlpacaLabs/protorepo-event-go v0.0.0-20200517012157-817122666c68
 	go.opentelemetry.io/otel v0.5.0
 	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
 	golang.org/x/sys v0.0.0-20190507160741-ecd444e8653b // indirect
--- trace.go
+++ trace.go
@@ -3,11 +3,15 @@ package kontext
 import (
 	"context"
 
+	eventV1 "github.com/AlpacaLabs/protorepo-event-go/alpacalabs/event/v1"
 	"go.opentelemetry.io/otel/api/trace"
 )
 
-func GetTraceID(ctx context.Context) string {
+func GetTraceInfo(ctx context.Context) eventV1.TraceInfo {
 	span := trace.SpanFromContext(ctx)
 	sc := span.SpanContext()
-	return sc.TraceID.String()
+	return eventV1.TraceInfo{
+		TraceId: sc.TraceID.String(),
+		Sampled: sc.IsSampled(),
+	}
 }
